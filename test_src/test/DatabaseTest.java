30d05036a830d6118703a90258e653b7bcb10f65
selyavkoivan <selyavkosh2@gmail.com>
2021-11-27 20:43:29 +0300

    unit testing of functions for product

---
+++ test_src/test/DatabaseTest.java
@@ -0,0 +1,47 @@
+package test;
+
+import com.google.gson.Gson;
+import org.junit.Test;
+import server.Database;
+import server.Models.Product;
+import server.Models.Size;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseTest {
+    @Test
+    public void testDatabaseAddProductWithoutSizes() {
+        Product product = new Product(0, "Хлопок", "Зеленый", "Логотип", 0,
+                "Converse Chuck Taylor High Top", "70 лучше", 180, "Кеды");
+        Database.getDatabase().addProduct(product.toString());
+    }
+    @Test
+    public void testDatabaseAddProduct() {
+        List<Size> sizes = new ArrayList<>();
+        sizes.add(new Size(0, "23", 32));
+        sizes.add(new Size(0, "43", 12));
+        Product product = new Product(0, "Хлопок", "Зеленый", "Логотип", 0,
+                "Converse Chuck Taylor High Top", "70 лучше", 180, "Кеды", sizes);
+        Database.getDatabase().addProduct(product.toString());
+    }
+    @Test
+    public void testDatabaseEditProduct()
+    {
+        List<Size> sizes = new ArrayList<>();
+        sizes.add(new Size(0, "23", 32));
+        sizes.add(new Size(0, "2", 12));
+        sizes.add(new Size(0, "43", 12));
+        Product product = new Product(0, "Хлопок", "Зеленый", "Логотип", 4,
+                "Converse Chuck Taylor Low Top", "70 лучше", 180, "Кеды", sizes);
+        Database.getDatabase().editProduct(product.toString());
+    }
+    @Test
+    public void testDatabaseDeleteProduct()
+    {
+        Product product = new Product(0, "", "", "", 4,
+                "", "", 180, "");
+        Database.getDatabase().deleteProduct(product.toString());
+    }
+}
